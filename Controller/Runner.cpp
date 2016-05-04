293a19810f5d48eed14efe8ef5fed70ca1492eb0
McKay Ririe <mrir2917@740s-pl-214.local>
2016-05-04 09:44:17 -0600

    Added code from graph structures in C++ video

--- Controller/Runner.cpp
+++ Controller/Runner.cpp
@@ -5,18 +5,18 @@
  *      Author: mrir2917
  */
 
-#include "Runner.h"
-#include "ArrayController.h"
+#include "NodeController.h"
+
 int main()
 {
-	double moreNumbers[5]
+    double moreNumbers[5];
 
 	for(int spot = 0; spot < 5; spot++)
 	{
 		moreNumbers[spot] = (spot * 34324 + 3948753984) % 2342.3;
 	}
 
-	ArrayController * baseController = new ArrayController();
+	NodeController * baseController = new NodeController();
 	baseController->start();
 	return 0;
 }
